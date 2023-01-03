4b846ac27f0eca2d98b79d4c9aeaa321fe80d793
Reza <reza1377maleki@gmail.com>
2023-01-03 20:40:03 +0330

    remove pseudo code

--- node.js
+++ node.js
@@ -15,14 +15,3 @@ export const nextBigNode = function(node) {
         return infiniteLeft()
     }
 }
-
-
-// check if node has a right child
-// if it doesnt check if it has a left child
-// if it has a left child return node's left child
-// if it doesnt have left child then return null
-// if it does have a right child then store it inside a variable called rightNode
-// make another function called infiniteLeft that gets rightNode as argument
-// check if node has a leftChild
-// if it doesn't return the node
-// if it does have a left child then call the infiniteLeft with the node's left child as argument 

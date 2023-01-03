8339b0d2fb0c205d87e13204a2ac96fc76f0b923
Reza <reza1377maleki@gmail.com>
2023-01-03 20:36:53 +0330

    clean the code and add find function to the tree

--- tree.js
+++ tree.js
@@ -24,58 +24,18 @@ export const tree = function(arr) {
             replaceParent = replaceData.parent
             replaceParent.data > replaceNode.data ? replaceParent.left = null : replaceParent.right = null;
         }
-        
-        
-        
         node.left !== null && node.left !== replaceNode ? replaceNode.left = node.left : replaceNode.left = null;
         node.right !== null && node.right !== replaceNode ? replaceNode.right = node.right : replaceNode.right = null;
-         
         prettyPrint(this.root)
     }
-    return {root, insert, remove}
+
+    const find = function(value, node = this.root, parent = 'root'){
+        return value === node.data ? {node, parent} :
+                value > node.data ? find(value, node.right, node) :
+                value < node.data ? find(value, node.left, node) : null
+    }
+    return {root, insert, remove, find}
 }
-
-
-const find = function(value, node = this.root, parent = 'root'){
-    return value === node.data ? {node, parent} :
-            value > node.data ? find(value, node.right, node) :
-            value < node.data ? find(value, node.left, node) : null
-}
-
-
-// call find function with the value as argument
-// store its value inside data variable
-// store data.node inside node variable
-// store data.parent inside parent variable
-// call nextBigNode function with node as its node
-// store its return value inside replace variable
-// if parent = this.root
-    // this.root = replace 
-// if else parent.data is less than node.data
-    // parent.right = replace
-// if else parent.data is more than node.data
-    // parent.left = replace
-// if node.left is not null and it's value not equal to replace.data
-    // replace.left = node.left
-    // else replace.left = null
-// if node.right is not null and it's value is not equal to replace.data
-    // replace.right = node.right
-    // else replace.right = null
-
-    
-
-
-
-// write a function named find that gets a value and  node = this.root and parent = this.root as arguments and returns the node and it's parent inside a object
-// check if value is equal to the data of the node
-    // if true then return the node and the parent
-// if value is more than data of the node 
-    // call the find function with node.right as node and node as parent
-// if value is less than the data of the node
-    // call find function with node.left as node and node as parent 
-
-
-
 
 // build a function that gets an array and create a balanced binary search tree and returns the root of the tree
 const buildTree = function(arr) {
